0d7e369426d2840107ccff31d4f3ea909898904e
oscar <osc-mcd@protonmail.com>
2026-07-09 01:04:51 +0100

    Prevent running the file without a terminal

--- src/main/java/net/approachcircle/app/Main.java
+++ src/main/java/net/approachcircle/app/Main.java
@@ -1,17 +1,27 @@
 package net.approachcircle.app;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.Console;
 
 public class Main {
     public static void main(String[] args) {
-        Utils.buildHitsoundNames();
-        Utils.findOsu();
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
             Logger.error("setting look and feel for UI failed");
             return;
         }
+        Console console = System.console();
+        if (console == null && !GraphicsEnvironment.isHeadless()){
+            JOptionPane.showMessageDialog(null,
+                    "Don't just double click this file, run it from the terminal! Refer to the GitHub page for more info.",
+                    "Silly billy",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        Utils.buildHitsoundNames();
+        Utils.findOsu();
         SkinModifier.findSkinFolder();
         while (true) {
             Logger.info("enter a name for the new skin: ");
